31b6b8ff7c7fb7aa4b3f7c701ababbfbb1c037ef
NateFBrassell <77899474+NateFBrassell@users.noreply.github.com>
2022-06-09 19:44:37 -0400

    Delete while_loops.py

--- while_loops.py
+++
@@ -1,6 +0,0 @@
-i = 1
-while i <= 10:
-	print(i)
-	i += 1
-print("Done with loop")	
-#specifying a loop condition, as long as that condition is true we will keep looping through until condition is false.
